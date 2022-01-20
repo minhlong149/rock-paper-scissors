5f680ce1c31e1f94f6e1d2524f504475dc231985
minhlong149 <minhlong9514@gmail.com>
2022-01-20 09:42:24 +0700

    Create playRound func

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-const shapes = ['Rock', 'Paper', 'Scissors'];
+const shapes = ["rock", "paper", "scissors", "rock"];
 
 function computerPlay() {
-  const randomIndex = Math.floor(Math.random() * shapes.length);
+  const randomIndex = Math.floor(Math.random() * (shapes.length - 1));
   return shapes[randomIndex];
 }
+
+function playRound(playerSelection, computerSelection) {
+  // playerSelection case-insensitive
+  playerSelection.toLowerCase();
+
+  if (playerSelection == computerSelection)
+    return `Tied! Both players choose ${playerSelection}`;
+
+	playerIndex = shapes.findIndex((shape) => shape == playerSelection);
+  if (shapes[playerIndex + 1] == computerSelection)
+    return `You lose! ${computerSelection} beat ${playerSelection}`;
+
+	return `You win! ${playerSelection} beat ${computerSelection}`; 
+}
+
+const playerSelection = computerPlay();
+const computerSelection = computerPlay();
+
+console.log(`Your choice: ${playerSelection}`);
+console.log(`Computer choice: ${computerSelection}`);
+
+console.log(playRound(playerSelection, computerSelection));
