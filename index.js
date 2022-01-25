06c5b1725165726d06623bf8d97c20f9b5fc37c3
minhlong149 <minhlong9514@gmail.com>
2022-01-25 22:24:50 +0700

    Complete

--- index.js
+++ index.js
@@ -1,61 +1,81 @@
+const startingScore = 0;
+
 const shapes = ["rock", "paper", "scissors", "rock"];
-let playerScore = (computerScore = 0); // startingScore
+let playerScore = (computerScore = startingScore);
 
 function randomPlay() {
   const randomIndex = Math.floor(Math.random() * (shapes.length - 1));
   return shapes[randomIndex];
 }
 
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection == computerSelection) {
-    return;
-  }
+function youWin(playerSelection, computerSelection) {
   const playerIndex = shapes.findIndex((shape) => shape == playerSelection);
+  
   if (shapes[playerIndex + 1] == computerSelection) {
     computerScore++;
-    return;
+    return false;
   }
+  
   playerScore++;
-  return;
+  return true;
 }
 
 const scoreboard = document.querySelector("#score");
-const gamePlay = document.querySelector("#game-play");
-
 const buttons = document.querySelectorAll(".btn");
+
+function preProcess() {
+  // reset previous color style
+  buttons.forEach((button) => {
+    button.setAttribute("class", "btn");
+  });
+}
+
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
-    const playerSelection = shapes[button.getAttribute("id")];
+    preProcess();
+
+    // move selection
+    const playerSelection = button.getAttribute("id");
     const computerSelection = randomPlay();
 
-    console.log(
-      `Your choice: ${playerSelection} | Computer choice: ${computerSelection}`
-    );
+    // target button selection
+    const yourButton = button;
+    const computerButton = document.querySelector(`#${computerSelection}`);
 
-    const test = playRound(playerSelection, computerSelection);
+    if (playerSelection == computerSelection) {
+      yourButton.classList.add("tie");
+      computerButton.classList.add("tie");
+    } else if (youWin(playerSelection, computerSelection)) {
+      yourButton.classList.add("win");
+      computerButton.classList.add("lose");
+    } else {
+      yourButton.classList.add("lose");
+      computerButton.classList.add("win");
+    }
+
     scoreboard.textContent = `${playerScore} - ${computerScore}`;
 
-    // if (playerScore === 5 || computerScore === 5) {
-    //   const announced = document.createElement("p");
-    //   announced.textContent = playerScore >= 5 ? "You win!" : "You lose :(";
+    if (playerScore === 5 || computerScore === 5) {
+      scoreboard.textContent = playerScore >= 5 ? "🏆 You win!" : "You lose 😞";
 
-    //   const container = document.querySelector("#results");
-    //   container.appendChild(announced);
+      buttons.forEach((button) => (button.style.display = "none"));
 
-    //   const choice = document.querySelectorAll(".btn");
-    //   choice.forEach((btn) => btn.remove());
+      const reset = document.createElement("div");
+      reset.classList.add("btn");
+      reset.style.padding = "1em";
+      reset.textContent = "Reset";
 
-    //   const reset = document.createElement("button");
-    //   reset.textContent = "Reset";
+      const container = document.querySelector("#choice");
+      container.appendChild(reset);
 
-    //   const btns = document.querySelector("#choice");
-    //   btns.appendChild(reset);
+      reset.addEventListener("click", () => {
+        playerScore = computerScore = startingScore;
+        scoreboard.textContent = `${playerScore} - ${computerScore}`;
 
-    //   // reset.addEventListener("click", () => {
-    //   //   playerScore = computerScore = startingScore;
-    //   //   scoreboard.remove();
-    //   //   gamePlay.remove();
-    //   // });
-    // }
+        buttons.forEach((button) => (button.style.display = "block"));
+        reset.remove();
+        preProcess();
+      });
+    }
   });
 });
